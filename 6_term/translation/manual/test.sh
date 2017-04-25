ae9831062352532abf6bbf2f05aacdd6536b5aa1
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2017-04-25 13:29:44 +0300

    Add more tests and .gitignore

--- 6_term/translation/manual/test.sh
+++ 6_term/translation/manual/test.sh
@@ -2,25 +2,28 @@
 make
 
 declare -a tests=('(a and b) or not (c xor (a or not b))'
+                  '((((((a and b) or not (c xor (a or not b)))))))'
+                  '((((((a and b) or not (c xor (a or not b))))))'
+                  '(((((a and b) or not (c xor (a or not b)))))))'
                   'a and b and c'
                   'a and b or c'
                   'a or b and c'
-						'a or b and c)')
+                  'a or b or c'
+                  '')
 
-#for test in ${tests[@]}
 for ((i = 0; i < ${#tests[@]}; i++))
 do
-	test="${tests[$i]}"
-	echo "TEST $test"
-	if echo $test | ./main $i
-	then
-		dot -Tsvg $i -o $i.svg
-		rm $i
-	else
-		echo "FAIL"
-	fi
-	echo
-	echo "=========="
-	echo
-	echo
+   test="${tests[$i]}"
+   echo "TEST $test"
+   if echo $test | ./main $i
+   then
+      dot -Tsvg $i -o $i.svg
+      rm $i
+   else
+      echo "FAIL"
+   fi
+   echo
+   echo "=========="
+   echo
+   echo
 done
